1b1d3034c985ae3ebc88b3429e59e679d0c6f6d8
Gihyeon <hamzzi99@naver.com>
2026-01-09 16:12:10 +0900

    네트워크모드로 전환

--- Plugins/MeshDeformation/Source/MeshDeformation/Public/Components/MDF_DeformableComponent.h
+++ Plugins/MeshDeformation/Source/MeshDeformation/Public/Components/MDF_DeformableComponent.h
@@ -10,7 +10,10 @@ class UDynamicMeshComponent;
 class UNiagaraSystem;
 class USoundBase;
 
-/** * [Step 6 최적화] 타격 데이터를 임시 저장하기 위한 구조체 */
+/** * [Step 6 최적화 -> Step 7-1 네트워크 확장] 
+ * 타격 데이터를 임시 저장 및 네트워크 전송하기 위한 구조체 
+ * TArray<FMDFHitData> 형태로 RPC 인자로 전달됩니다.
+ */
 USTRUCT(BlueprintType)
 struct FMDFHitData
 {
@@ -34,7 +37,7 @@ struct FMDFHitData
 };
 
 UCLASS(Blueprintable, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
-class MESHDEFORMATION_API UMDF_DeformableComponent : public UActorComponent
+class HELLUNA_API UMDF_DeformableComponent : public UActorComponent
 {
     GENERATED_BODY()
 
@@ -48,8 +51,20 @@ protected:
     UFUNCTION()
     virtual void HandlePointDamage(AActor* DamagedActor, float Damage, class AController* InstigatedBy, FVector HitLocation, class UPrimitiveComponent* FHitComponent, FName BoneName, FVector ShotFromDirection, const class UDamageType* DamageType, AActor* DamageCauser);
 
-    /** [Step 6 최적화] 모인 타격 지점들을 한 프레임의 끝에서 한 번에 연산 */
+    /** * [Step 6 최적화] 모인 타격 지점들을 한 프레임의 끝에서 한 번에 연산 
+     * (서버에서만 호출되어 RPC를 발송하는 역할로 변경 예정)
+     */
     void ProcessDeformationBatch();
+
+    /**
+     * [Step 1 네트워크 추가]
+     * 서버에서 배칭된 타격 데이터를 모든 클라이언트에게 전송하여 실행합니다.
+     * Reliable: 지형 변형은 게임플레이(엄폐물 제거 등)에 영향을 주므로 패킷 유실을 허용하지 않습니다.
+     * NetMulticast: 서버 -> 모든 클라이언트 (서버 자신 포함 가능, 구현부에 따라 처리)
+     * * @param BatchedHits - 서버에서 0.1초(BatchProcessDelay)간 모은 타격 데이터 배열
+     */
+    UFUNCTION(NetMulticast, Reliable)
+    void NetMulticast_ApplyDeformation(const TArray<FMDFHitData>& BatchedHits);
 
 public:
     /** 원본으로 사용할 StaticMesh 에셋 */
